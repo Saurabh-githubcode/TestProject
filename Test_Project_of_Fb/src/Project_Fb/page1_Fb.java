23d79645b9373e5abaee75ea1f3fc5b5dc4719e7
HP <HP@DESKTOP-9UKQKVU>
2023-03-29 09:19:52 +0530

    {Here I have written code for the automate Fb page}

--- Test_Project_of_Fb/src/Project_Fb/page1_Fb.java
+++ Test_Project_of_Fb/src/Project_Fb/page1_Fb.java
@@ -1,6 +1,16 @@
 package Project_Fb;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class page1_Fb
 {
 // Automate Fb page
+	
+     public static void main(String args [] ) 
+    {
+      System.setProperty("Webdriver.Chrome.Driver", "C:\\Users\\HP\\Browser Extention\\chromedriver.exe");
+   	  WebDriver driver=new ChromeDriver();
+   	  driver.get("https://www.Facebook.com");
+	}
 }
